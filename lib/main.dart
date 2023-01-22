77ccc75387d9c45b32b45b734410aa61c44d2048
Abdelillah <atamossat@gmail.com>
2023-01-22 22:39:13 +0100

    Create Notes View

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:first_flutter_app/views/login_view.dart';
 import 'package:first_flutter_app/views/register_view.dart';
+import 'package:first_flutter_app/views/verfiy_email_view.dart';
 import 'package:flutter/material.dart';
-
 import 'firebase_options.dart';
+import 'dart:developer' as devtools show log;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -32,7 +33,16 @@ class HomePage extends StatelessWidget {
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
-              return LoginView();
+              final user = FirebaseAuth.instance.currentUser;
+              if (user != null) {
+                if (user.emailVerified) {
+                  return const NotesView();
+                } else {
+                  return const VerifyEmailView();
+                }
+              } else {
+                return const LoginView();
+              }
             default:
               return const CircularProgressIndicator();
           }
@@ -40,4 +50,34 @@ class HomePage extends StatelessWidget {
   }
 }
 
+enum MenuAction { logout }
 
+class NotesView extends StatefulWidget {
+  const NotesView({Key? key}) : super(key: key);
+
+  @override
+  State<NotesView> createState() => _NotesViewState();
+}
+
+class _NotesViewState extends State<NotesView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main UI'),
+        actions: [
+          PopupMenuButton<MenuAction>(
+              onSelected: (value) {
+                devtools.log(value.name);
+              },
+              itemBuilder: (context) {
+                return const [
+                  PopupMenuItem<MenuAction>(
+                      value: MenuAction.logout, child: Text('Log out'))
+                ];
+              })
+        ],
+      ),
+    );
+  }
+}
