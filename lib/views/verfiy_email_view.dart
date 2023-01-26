c08b426d5ce298524d3e053e8588c4a2c77e8435
Abdelillah <atamossat@gmail.com>
2023-01-26 18:29:31 +0100

    Add firebase auth provider and auth service that covers firebase_auth_provider.dart

--- lib/views/verfiy_email_view.dart
+++ lib/views/verfiy_email_view.dart
@@ -1,5 +1,5 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:first_flutter_app/constants/routes.dart';
+import 'package:first_flutter_app/services/auth/auth_service.dart';
 import 'package:flutter/material.dart';
 
 class VerifyEmailView extends StatefulWidget {
@@ -24,13 +24,12 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
               "If you haven't received a verification email yet, press the button below"),
           TextButton(
               onPressed: () async {
-                final user = FirebaseAuth.instance.currentUser;
-                await user?.sendEmailVerification();
+                await AuthService.firebase().sendEmailVerification();
               },
               child: const Text('Verify your email')),
           TextButton(
               onPressed: () async {
-                await FirebaseAuth.instance.signOut();
+                await AuthService.firebase().logout();
                 Navigator.of(context).pushNamedAndRemoveUntil(registerRoute, (route) => false);
               },
               child: const Text('Restart'))
