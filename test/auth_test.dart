e9ff862205e3995748b86c0da67e16dc51f33a81
Abdelillah <atamossat@gmail.com>
2023-02-12 18:54:21 +0100

    step-19

--- test/auth_test.dart
+++ test/auth_test.dart
@@ -104,7 +104,7 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializedException();
     if (email == 'foo@bar.com') throw UserNotFoundAuthException();
     if (password == 'foobar') throw WrongPasswordAuthException();
-    const user = AuthUser(email: 'foo@bar.com', isEmailVerified: false);
+    const user = AuthUser(id: 'my_id',email: 'foo@bar.com', isEmailVerified: false);
     _user = user;
     return Future.value(user);
   }
@@ -122,7 +122,7 @@ class MockAuthProvider implements AuthProvider {
     if (!_isInitialized) throw NotInitializedException();
     final user = _user;
     if (user == null) throw UserNotFoundAuthException();
-    const newUser = AuthUser(email: 'foo@bar.com', isEmailVerified: true);
+    const newUser = AuthUser(id: 'my_id',email: 'foo@bar.com', isEmailVerified: true);
     _user = newUser;
   }
 }
